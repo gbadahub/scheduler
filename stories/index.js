ad287cc757d8052a9313cb47557a30f2e988cbf0
Georgina Bada <georginabada@gmail.com>
2022-01-20 14:56:20 -0500

    Creating show, confirm, status,and error components

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,10 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show"
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error"
 
 // Buttons
 storiesOf("Button", module)
@@ -141,6 +145,12 @@ storiesOf("Appointment", module)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm"/> )
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () =><Show onEdit={action("onEdit")} onDelete={action("onDelete")} /> )
+  .add("Confirm", () => <Confirm onCancel={action("onCancel")} onConfirm={action("onConfirm")} />)
+  .add("Status", () => <Status/>)
+  .add("Error", () => <Error onClose={action("onClose")}/>)
+  
+  
   
 
 
